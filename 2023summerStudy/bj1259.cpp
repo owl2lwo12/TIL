8ff20e466e0ed9b6427c85ed42d8d6297160edca
owl2lwo12 <owl2lwo12@ajou.ac.kr>
2023-07-12 20:15:34 +0900

    today

---
+++ 2023summerStudy/bj1259.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	bool isPel = false;
+	while (true) {
+		string num;
+		getline(cin, num);
+		isPel = true;
+
+		if (num[0] == '0') {
+			break;
+		}
+		else {
+			for (int i = 0; i < num.length() / 2; i++) {
+				if (num[i] != num[num.length()-1-i]) {
+					isPel = false;
+					break;
+				}
+			}
+		}
+		if (isPel == true) cout << "yes" << endl;
+		else cout << "no" << endl;
+	}
+}
---
+++ 2023summerStudy/bj2609.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+	int tnum1, tnum2;
+	int glb, lub;
+	cin >> num1 >> num2;
+	tnum1 = num1;
+	tnum2 = num2;
+	
+	while (true) {
+		if (num2 > num1) swap(num1, num2);
+
+		if (num1 % num2 != 0) {
+			int t = num1 % num2;
+			num1 = num2;
+			num2 = t;
+		}
+		else {
+			glb = num2;
+			break;
+		}
+	}
+	lub = tnum1 * (tnum2 / glb);
+
+	cout << glb << endl << lub;
+
+}
