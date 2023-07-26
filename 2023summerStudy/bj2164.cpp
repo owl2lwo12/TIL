589a86196ee4fa69f52ff817297814c7437ff78c
owl2lwo12 <owl2lwo12@ajou.ac.kr>
2023-07-27 00:38:23 +0900

    7/26 work

---
+++ 2023summerStudy/bj2164.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+	if (N == 1) cout << "1";
+	else {
+		N -= 1;
+		int i = 1;
+		while (N > i) {
+			N -= i;
+			i *= 2;
+		}
+		N = 2 * N;
+		cout << N;
+	}
+}
